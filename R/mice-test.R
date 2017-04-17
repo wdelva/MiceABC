5f8289c3fa7b672e66dc048685dc5ac689e772a9
wdelva <wimdelva@gmail.com>
2017-04-17 13:39:24 +0200

    Testing whether giving MICE more data helps. Under the new algorithm, the closeness gets better and better. Next question will be if/how to declare a stopping criterion.

--- R/mice-test.R
+++ R/mice-test.R
@@ -24,21 +24,24 @@ nl.path <- "/Applications/NetLogo 6.0/app/"
 NLStart(nl.path, gui=FALSE, nl.obj="agemix.nl1", nl.jarname = 'netlogo-6.0.0.jar')
 model.path <- "/Users/delvaw/Google Drive/IBM course/tutorials/AgeMixing/Calibration/dummy.0.1.nlogo"
 NLLoadModel(model.path, nl.obj="agemix.nl1")
-master.data <- agemix.simulate(param.set = c(8, 0.2), no.repeated.sim=5)
+setwd("/Users/delvaw/Documents/MiceABC/R")
+master.data <- agemix.simulate(param.set = c(8, 0.2), no.repeated.sim=25)
 
 targets <- c(48.41, 27.91908, 54.22174)
-n.experiments <- 40
-maxiter <- 2
-reps <- 1
-alpha <- 0.5
-rel.dist.tol <- 0.5
-rel.dist.tol.final <- 0.01
+n.experiments <- 60
+maxit <- 20
+maxiter <- 6
+reps <- 5
+alpha <- 0.7
+rel.dist.tol <- 0.000001
+rel.dist.tol.final <- 0.005
 
 test.list <- MiceABC(targets = targets,
                     n.experiments = n.experiments,
-                    lls = c(5, 0.15),
-                    uls = c(10, 0.25),
+                    lls = c(2, 0.05),
+                    uls = c(10, 0.65),
                     model = model.wrapper,
+                    maxit = maxit,
                     maxiter = maxiter,
                     reps = reps,
                     alpha = alpha,
@@ -58,20 +61,25 @@ last.one.selected <- round(shortest.dist.percentile * length(dist.order))
 selected.distances <- dist.order[1:last.one.selected]
 all.data.selected <- all.data[selected.distances, ]
 all.data.filtered <- dplyr::filter(all.data,
-                            abs(all.data$cumsum.transmissions - targets[1]) / targets[1] <= rel.dist.tol.final,
-                            abs(all.data$mean.age.hivpos - targets[2]) / targets[2] <= rel.dist.tol.final,
-                            abs(all.data$var.age.hivpos - targets[3]) / targets[3] <= rel.dist.tol.final)
+                                   sum.sq.rel.dist <= 0.00346296164344922)#test.list$rel.dist.cutoff[length(test.list$rel.dist.cutoff)])
+                            # abs(all.data$cumsum.transmissions - targets[1]) / targets[1] <= rel.dist.tol.final,
+                            # abs(all.data$mean.age.hivpos - targets[2]) / targets[2] <= rel.dist.tol.final,
+                            # abs(all.data$var.age.hivpos - targets[3]) / targets[3] <= rel.dist.tol.final)
 
 all.data.choices <-list(all.data.selected, all.data.filtered)
 all.data.choice.index <- ifelse(nrow(all.data.selected) > nrow(all.data.filtered),
                                 1,
                                 2)
-all.data.final <- all.data.choices[[all.data.choice.index]]
-calibration.list$estimates <- all.data.final
+all.data.final <- all.data.choices[[2]] # all.data.choices[[all.data.choice.index]]
+test.list$estimates <- all.data.final
 
 summary(all.data.final)
+pairs(all.data.final[,c(2:4, 6:7)])
 pairs(all.data.final[,6:7])
+var(all.data.final[,6:7])
+
+
 dim(all.data.final[,6:7])
 
-save.image(file = "/Users/delvaw/Google Drive/IBM course/tutorials/AgeMixing/Calibration/Thursday.evening.RData")
+save.image(file = "/Users/delvaw/Google Drive/IBM course/tutorials/AgeMixing/Calibration/Sunday.evening.RData")
 
